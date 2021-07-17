d5a1e111e79783daa4174e73cd6639c65c5068b9
Paul Mayer <paul@paul-mayer.eu>
2021-07-17 17:52:32 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Knative Terraform Example
+## Init
+```
+    terraform apply
+```
---
+++ eks-cluster.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.20"
+  subnets         = module.vpc.private_subnets
+
+  vpc_id = module.vpc.vpc_id
+
+  workers_group_defaults = {
+    root_volume_type = "gp2"
+  }
+
+  worker_groups = [
+    {
+      name                 = "example-cluster-worker"
+      instance_type        = "t3.nano"
+      #additional_userdata  = "echo foo bar"
+      asg_desired_capacity = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
